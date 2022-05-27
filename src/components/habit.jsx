22afcb25f6704c27074dde4459e15f6f6bbaffcf
Bella <shdudtnr3939@naver.com>
2022-05-27 23:11:51 +0900

    ADD : callback function

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 
 export default class habit extends Component {
-  state = {
-    count: 0,
-  };
 
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야함
-    this.setState({count : this.state.count + 1});
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecrement = () => {
-    const count = this.state.count -1 ;
-    this.setState({count : count < 0 ? 0 : count});
+    this.props.onDecrement(this.props.habit);
   };
 
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  }
+
+
   render() {
-    console.log(this.props.habit);
     const {name, count} = this.props.habit;
     return (
       <li className='habit'>
@@ -28,7 +27,7 @@ export default class habit extends Component {
         <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
           <i className='fas fa-minus-square'></i>
         </button>
-        <button className='habit-button habit-delete'>
+        <button className='habit-button habit-delete' onClick={this.handleDelete}>
           <i className='fas fa-trash'></i>
         </button>
       </li>
