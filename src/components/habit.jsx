d9b87234778de48ced007090f8022bb424b7171f
Bella <shdudtnr3939@naver.com>
2022-08-15 16:36:54 +0900

    220815 callback method completed

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -2,19 +2,16 @@ import React, { Component } from 'react'
 
 export default class Habit extends Component {
 
-  state = {
-    count: 0
-  }
-
   handleIncrement = () => {
-    // state object 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
-    this.setState({count: this.state.count+1});
+    this.props.onIncrement(this.props.habit);
   }
 
   handleDecrement = () => {
-    // state object 안에 있는 count를 감소한 뒤 state를 업데이트 해야 함.
-    const count = this.state.count -1;
-    this.setState({count: count < 0 ? 0 : count});
+    this.props.onDecrement(this.props.habit);
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   }
 
   render() {
@@ -30,7 +27,7 @@ export default class Habit extends Component {
       <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
         <i className="fas fa-minus-square"></i>
       </button>
-      <button className='habit-button habit-delete'>
+      <button className='habit-button habit-delete' onClick={this.handleDelete}>
         <i className="fas fa-trash"></i>
       </button>
     </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -10,12 +10,36 @@ class Habits extends Component {
     ]
   }
 
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits }); // {habits : habits } key : value가 같은 경우 생략 가능
+  }
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  }
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter( item => item.id !== habit.id);
+    this.setState( { habits });
+  }
+
   render() {
     return (
       <ul>
         {
           this.state.habits.map(habit => 
-            <Habit key={habit.id}  habit={habit} />
+            <Habit key={habit.id}  habit={habit} 
+            onIncrement={this.handleIncrement} 
+            onDecrement={this.handleDecrement} 
+            onDelete={this.handleDelete}/>
           )
         }
       </ul>
